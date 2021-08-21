b74c51249f13e94922cdefcd2d9e4fad918f55a4
Tshepo Masuku <tshepomasuku16@gmail.com>
2021-08-21 13:17:25 +0200

    get images modal creation

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal } from 'react-responsive-modal';
 
 function ImageLinkForm({ onInputChange, onPictureSubmit }) {
+
+    const [ openModal, setOpenModal ] = useState(false);
+    const onOpenModal = () => { setOpenModal(true) }
+    const onCloseModal = () => { setOpenModal(false) }
+
     return (
         <div>
             <p className='f4 black'>
@@ -22,9 +28,60 @@ function ImageLinkForm({ onInputChange, onPictureSubmit }) {
                         style={{ background: 'linear-gradient(to right, rgb(228, 110, 6), rgb(239, 5, 5))' }}
                         onClick={onPictureSubmit}
                     >
-                        Detect
+                        Detect Face
                     </button>
-                </div>                
+                    <button 
+                        className='w-30 grow f4 link ph3 pv2 dib white'
+                        style={{ background: 'linear-gradient(to right, rgb(228, 110, 6), rgb(239, 5, 5))' }}
+                        onClick={ onOpenModal }
+                    >
+                        Get Images
+                    </button>
+                </div>  
+                <Modal 
+                    open={openModal} 
+                    onClose={ onCloseModal }
+                    closeOnOverlayClick={ onCloseModal }
+                    closeOnEsc={ onCloseModal }
+                    classNames={{
+                    overlay: 'customOverlay',
+                    modal: 'customModal',
+                    }}
+                >
+                    <div className="text-center">
+                        <h3 className="monoton"> 
+                            Select any website from the buttons below.
+                        </h3>
+                        <p className="monoton">
+                            On the website you have selected, 
+                            you must choose any image with a face 
+                            and copy its image-address 
+                            then paste it on feycbox so you can detect the image.
+                        </p> 
+
+                        <button 
+                            variant="primary" 
+                            onClick={ onCloseModal }
+                            className="mx-2 mt-2 mb-5"
+                        >
+                            <a href="http://pixabay.com/images/search/faces/"> 
+                                pixabay 
+                            </a>
+                        </button>
+                        
+                        <button 
+                            variant="danger" 
+                            onClick={ onCloseModal }
+                            className="mx-2 mt-2 mb-5"
+                        >
+                            <a href="http://www.pexels.com/search/face/"> 
+                                pexels 
+                            </a>
+                        </button>
+
+                        <h6> THANX A LOT FOR USING feycbox, WITH LOVE FROM RSA :~) </h6>
+                    </div>
+                </Modal>            
             </div>
         </div>
     )
