7d6ac9dc6fe8797f4225f9b6c8425e368f363ddb
TshepoMasuku <tshepomasuku16@gmail.com>
2025-12-03 23:22:03 +0200

    touched base_api_url

--- src/api-base_url.js
+++ src/api-base_url.js
@@ -1,4 +1,4 @@
-// react_app_env = "prod" || "dev";
+// react app environment(react_app_env) can be either = "prod" || "dev";
 const react_app_env = "prod";
 const BASE_API_URL = react_app_env === "prod" ? 
     "https://feycback-api.vercel.app" : "http://localhost:3001";
