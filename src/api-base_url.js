52eb05e15b4c554fe929419377f4aceb98ac02e7
TshepoMasuku <tshepomasuku16@gmail.com>
2026-01-01 12:25:34 +0200

    updated base API URL

--- src/api-base_url.js
+++ src/api-base_url.js
@@ -1,6 +1,6 @@
 // react app environment(react_app_env) can be either = "prod" || "dev";
 const react_app_env = "prod";
 const BASE_API_URL = react_app_env === "prod" ? 
-    "https://feycbaackk-api.vercel.app" : "http://localhost:3001";
+    "https://feycbaackk-patient-river-1261.fly.dev" : "http://localhost:3001";
 
 export default BASE_API_URL;
